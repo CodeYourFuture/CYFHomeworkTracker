3b6ab947db5b830f86a2be1f49304645ee70b18d
Chris Owen <cowen1992@hotmail.co.uk>
2020-11-05 21:39:18 +0000

    Update HomeworkRepositories.js

--- src/config/HomeworkRepositories.js
+++ src/config/HomeworkRepositories.js
@@ -1,1 +1,14 @@
-export default ["html-css-project", "js-exercises", "HTML-CSS-Homework"];
+export default [
+  "HTML-CSS-Coursework-Week1",
+  "HTML-CSS-Coursework-Week2",
+  "HTML-CSS-Coursework-Week3",
+  "JavaScript-Core-1-Coursework-Week1",
+  "JavaScript-Core-1-Coursework-Week2",
+  "JavaScript-Core-1-Coursework-Week3",
+  "JavaScript-Core-2-Coursework-Week1",
+  "JavaScript-Core-2-Coursework-Week2",
+  "JavaScript-Core-2-Coursework-Week3",
+  "JavaScript-Core-3-Coursework-Week1",
+  "JavaScript-Core-3-Coursework-Week2",
+  "JavaScript-Core-3-Coursework-Week3",
+];
